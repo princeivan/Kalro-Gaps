ca7c39231c51cc848096dbff20e3f96f9c032cff
mercy chebet <princeivan733@gmail.com>
2024-09-09 21:05:05 +0300

    second commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,8 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 
 import os  
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,7 +31,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-GOOGLE_CLOUD_TRANSLATE_API_KEY = os.getenv('AIzaSyA_3ymIz3S_ca2993dkHYPHie2o_nkAVsA')
+
 
 # REST_FRAMEWORK = {
 #     'DEFAULT_PAGINATION_CLASS': 'api.apps.core.pagination.LinkHeaderPagination',
@@ -96,8 +98,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PWD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT"),
     }
 }
 
--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -28,8 +28,20 @@ const Home = ({filtereddata}) => {
   if (isLoading) {
     return (
       <div className="loading-container">
-        <CirclesWithBar color="#00BFFF" height={80} width={80} />
-        <h1>Loading...</h1>
+       <CirclesWithBar
+  height="100"
+  width="100"
+  color="#4fa94d"
+  outerCircleColor="#4fa94d"
+  innerCircleColor="#4fa94d"
+  barColor="#4fa94d"
+  ariaLabel="circles-with-bar-loading"
+  wrapperStyle={{}}
+  wrapperClass=""
+  visible={true}
+
+  />
+      
       </div>
     );
   }
