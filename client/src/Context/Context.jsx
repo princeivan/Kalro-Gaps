12fa27607e69a04b82cfc3704d13c36dfc55da07
mercy chebet <princeivan733@gmail.com>
2024-09-10 08:09:47 +0300

    third

--- client/src/Context/Context.jsx
+++ client/src/Context/Context.jsx
@@ -1,34 +1,32 @@
 import { createContext, useEffect, useState } from "react";
 
+export const GlobalContext = createContext(null);
 
-export const GlobalContext = createContext(null)
+export default function GlobalState({ children }) {
+  const [gaps, setGaps] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-export default function GlobalState({ children}){
-    const [gaps, setGaps] = useState([])
-    const [isLoading, setIsLoading] = useState(false)
-   
+  useEffect(() => {
+    getGaps();
+  }, []);
 
+  async function getGaps() {
+    try {
+      setIsLoading(true);
+      const response = await fetch("http://127.0.0.1:8000/api/");
+      const data = await response.json();
 
-    useEffect(()=> {
-      getGaps()
-    }, [])
-
-    async function getGaps(){
-        try {
-          setIsLoading(true)
-          const response = await fetch('http://127.0.0.1:8000/api/')
-          const data = await response.json()
-       
-        setGaps(data) 
-        } catch (error) {
-          console.log(error);
-          
-        }finally{
-          setIsLoading(false)
-        }
-     
+      setGaps(data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
     }
+  }
 
-   
-  return <GlobalContext.Provider  value= {{gaps, setGaps, isLoading}} >{children}</GlobalContext.Provider>
+  return (
+    <GlobalContext.Provider value={{ gaps, setGaps, isLoading }}>
+      {children}
+    </GlobalContext.Provider>
+  );
 }
