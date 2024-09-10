21a5cafbb0c08252fb090c2485c168b69965dc0e
mercy chebet <princeivan733@gmail.com>
2024-09-10 10:44:28 +0300

    fourth

--- server/server/settings.py
+++ server/server/settings.py
@@ -15,7 +15,6 @@ from dotenv import load_dotenv
 
 import os  
 load_dotenv()
-os.environ['DJANGO_SETTINGS_MODULE'] = 'server.settings'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
