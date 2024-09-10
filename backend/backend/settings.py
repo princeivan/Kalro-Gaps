512587aa486e59f27cb386334d6d604162768297
mercy chebet <princeivan733@gmail.com>
2024-09-10 09:45:45 +0300

    fourth

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-300yl-9(h70mc82x(xun_a^8q9%ers@af%(glx^40gs#z@u_z7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
