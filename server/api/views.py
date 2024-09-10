4cbebd0d499be1ea5107a09df4bcfac1a819121e
mercy chebet <princeivan733@gmail.com>
2024-09-10 11:24:03 +0300

    fourth

--- server/api/views.py
+++ server/api/views.py
@@ -2,13 +2,8 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import  csrf_exempt
 from rest_framework.parsers import JSONParser 
-import requests
 from rest_framework.response import Response 
 from rest_framework.decorators import api_view
-
-
-
-
 
 
 from  .models import Gaps
