27c45c9c41f7c493c7b2d6eacf6e51e434d63fc9
mercy chebet <princeivan733@gmail.com>
2024-09-10 09:04:13 +0300

    thir

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,53 +1,51 @@
-import React, { useContext, useState } from 'react'
-import { useTranslation } from 'react-i18next';
-import { GlobalContext } from '../../Context/Context';
-import './Home.css'
-import { Link } from 'react-router-dom';
-import { CirclesWithBar } from 'react-loader-spinner'
+import React, { useContext, useState } from "react";
+import { useTranslation } from "react-i18next";
+import { GlobalContext } from "../../Context/Context";
+import "./Home.css";
+import { Link } from "react-router-dom";
+import { CirclesWithBar } from "react-loader-spinner";
 
-const Home = ({filtereddata}) => {
+const Home = ({ filtereddata }) => {
   const { t } = useTranslation();
-  const {isLoading} = useContext(GlobalContext)
+  const { isLoading } = useContext(GlobalContext);
   const [selected, setSelected] = useState(null);
-  const [currentPage , setCurrentPage] = useState(1)
-
+  const [currentPage, setCurrentPage] = useState(1);
 
   const itemsPerPage = 6;
-  const indexOfLastItem = currentPage * itemsPerPage ;
+  const indexOfLastItem = currentPage * itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
   const currentItems = filtereddata.slice(indexOfFirstItem, indexOfLastItem);
 
-  const toggleReadMore =(getCurrentId)=>{
-   setSelected((prevSelected) => (prevSelected === getCurrentId ? null : getCurrentId))
-  
-  }
-  const paginate = (pageNumber) =>{
-    setCurrentPage(pageNumber)
-  }
+  const toggleReadMore = (getCurrentId) => {
+    setSelected((prevSelected) =>
+      prevSelected === getCurrentId ? null : getCurrentId
+    );
+  };
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
 
   if (isLoading) {
     return (
       <div className="loading-container">
-       <CirclesWithBar
-  height="100"
-  width="100"
-  color="#4fa94d"
-  outerCircleColor="#4fa94d"
-  innerCircleColor="#4fa94d"
-  barColor="#4fa94d"
-  ariaLabel="circles-with-bar-loading"
-  wrapperStyle={{}}
-  wrapperClass=""
-  visible={true}
-
-  />
-      
+        <CirclesWithBar
+          height="100"
+          width="100"
+          color="#4fa94d"
+          outerCircleColor="#4fa94d"
+          innerCircleColor="#4fa94d"
+          barColor="#4fa94d"
+          ariaLabel="circles-with-bar-loading"
+          wrapperStyle={{}}
+          wrapperClass=""
+          visible={true}
+        />
       </div>
     );
   }
   return (
     <div>
-      <h1>{t('Gaps By Value Chain')}</h1>
+      <h2>{t("Gaps By Value Chain")}</h2>
       <div className="card-container">
         {currentItems.length > 0 ? (
           currentItems.map(({ id, image, title, description }, index) => (
@@ -59,36 +57,40 @@ const Home = ({filtereddata}) => {
               </div>
               <div className="card-content">
                 <h2 className="card-title">
-                  <Link style={{ color: 'black' }} to={`/gaps-detail/${id}/`}>
+                  <Link style={{ color: "black" }} to={`/gaps-detail/${id}/`}>
                     {title}
                   </Link>
                 </h2>
                 <p className="card-description">
-                  {selected === id ? description : `${description.substring(0, 100)}...`}
+                  {selected === id
+                    ? description
+                    : `${description.substring(0, 100)}...`}
                 </p>
                 <button onClick={() => toggleReadMore(id)}>
-                  {selected === id ? 'Show less' : 'Read More'}
+                  {selected === id ? "Show less" : "Read More"}
                 </button>
               </div>
             </div>
           ))
         ) : (
           <p>No results found</p>
-        )} 
-      </div> 
+        )}
+      </div>
       <div className="pagination">
-        {Array.from({ length: Math.ceil(filtereddata.length / itemsPerPage) }).map((_, index) => (
+        {Array.from({
+          length: Math.ceil(filtereddata.length / itemsPerPage),
+        }).map((_, index) => (
           <button
             key={index}
             onClick={() => paginate(index + 1)}
-            className={currentPage === index + 1 ? 'active' : ''}
+            className={currentPage === index + 1 ? "active" : ""}
           >
             {index + 1}
           </button>
         ))}
-      </div>      
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
