c1c7972a3b1180b65048ce7ce610c73f106e72ee
mercy chebet <princeivan733@gmail.com>
2024-09-11 09:06:50 +0300

    fourth

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -9,10 +9,16 @@ import { Link } from "react-router-dom";
 import ScrollProgressBar from "../ScrollProgressBar/ScrollProgressBar";
 import { GlobalContext } from "../../Context/Context";
 import Gaps from "../Gaps/Gaps";
+import { HiOutlineMenuAlt3 } from "react-icons/hi";
 
+const activeLink = ({ isActive }) => {
+  isActive ? `active` : `notactive`;
+};
 const Navbar = ({ handleInputChange, handleKeyDown, searchInput }) => {
   const { t } = useTranslation();
   const [scrollPage, setScrollPage] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
+  const [menu, setMenu] = useState("");
 
   const fixedNavbar = () => {
     if (window.scrollY > 50) {
@@ -23,8 +29,16 @@ const Navbar = ({ handleInputChange, handleKeyDown, searchInput }) => {
   };
   window.addEventListener("scroll", fixedNavbar);
 
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  const hideMenu = () => {
+    setShowMenu(false);
+  };
+
   return (
-    <nav>
+    <header>
       {/* Top Navigation Section */}
       <div className="top-nav">
         <ul>
@@ -51,34 +65,59 @@ const Navbar = ({ handleInputChange, handleKeyDown, searchInput }) => {
         </h3>
         <h3 className="logo">GAPS for Selected Value Chain</h3>
       </div>
+      <nav className={showMenu ? "show-nav" : "hide-nav"}>
+        <div
+          className={showMenu ? `nav-wrapper show-nav-wrapper` : `nav-wrapper`}
+          onClick={hideMenu}
+        >
+          <div className={scrollPage ? " main-nav fixed" : "main-nav"}>
+            <ul>
+              <Link
+                to="/"
+                className={activeLink}
+                onClick={() => setMenu("Home")}
+              >
+                <li>{t("Home")}</li>
+              </Link>
+              <Link
+                to="/about"
+                className={activeLink}
+                onClick={() => setMenu("About")}
+              >
+                <li>{t("About")}</li>
+              </Link>
+              <Link
+                to="/gaps"
+                className={activeLink}
+                onClick={() => setMenu("Gaps By Value Chain")}
+              >
+                <li>{t("Gaps By Value Chain")}</li>
+              </Link>
+              <Link
+                to="/Contact"
+                className={activeLink}
+                onClick={() => setMenu("Contact")}
+              >
+                <li>{t("Contact")}</li>
+              </Link>
 
-      <div className={scrollPage ? " main-nav fixed" : "main-nav"}>
-        <ul>
-          <Link to="/">
-            <li>{t("Home")}</li>
-          </Link>
-          <Link to="/about">
-            <li>{t("About")}</li>
-          </Link>
-          <Link to="/gaps">
-            <li>{t("Gaps By Value Chain")}</li>
-          </Link>
-          <Link to="/Contact">
-            <li>{t("Contact")}</li>
-          </Link>
-
-          <li>
-            <input
-              type="text"
-              placeholder="search Value Chain.."
-              value={searchInput}
-              onChange={handleInputChange}
-              onKeyDown={handleKeyDown}
-            />
-          </li>
-        </ul>
+              <li>
+                <input
+                  type="text"
+                  placeholder="search Value Chain.."
+                  value={searchInput}
+                  onChange={handleInputChange}
+                  onKeyDown={handleKeyDown}
+                />
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <div className="menu-icon">
+        <HiOutlineMenuAlt3 size={28} onClick={toggleMenu} />
       </div>
-    </nav>
+    </header>
   );
 };
 
